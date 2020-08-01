8094d5380d92643ef2b2e61db3f623566a91bbd8
vonnAlves <alberto420762@gmail.com>
2020-07-31 23:04:32 -0400

    Adicionando funcionalidade

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-
-
-const nomeDaEscola ='Alphatech highschool';
-
 const alunosDaEscola=[
 {       
     Nome:"Henrique",
@@ -36,61 +31,110 @@ const alunosDaEscola=[
 },
 {   
     Nome:"Lucca",
-    nNotas:[10,9.8,9.6],
-    ursos:[{NomeDoCurso:"UX",dataMatricula:new Date}],
+    Notas:[10,9.8,9.6],
+    Cursos:[{NomeDoCurso:"UX",dataMatricula:new Date}],
     Faltas:0
 }];
 
-//funçao para adicionar aluno no sistema escolar, e retorna um feedback caso aluno seja inserido corretamente;
+//adiciona aluno no sistema escolar, e retorna um feedback caso aluno seja inserido corretamente;
 const adicionarAluno = Nome => {
     let novoAluno = {
        Nome: Nome,
+       Notas:[],
+       Cursos:[],
+       Faltas:0
     };
-console.log("Aluno Cadastrado:" + Nome)
+console.log("Aluno:" + Nome + " Cadastrado com sucesso!");
 console.log("")
 
 alunosDaEscola.push(novoAluno);
 };
-
 //adicionarAluno("Carlos Alberto");
 
-//lista todos os alunos contidos na array;
+//percorre o array e imprime todos os alunos contidos em um formato amigável;
 const listarAlunos = ()=> {
     alunosDaEscola.forEach((aluno) => {
-        console.log("-".repeat(25));
-        console.log("Nome Do Aluno:" + aluno.Nome)
-        console.log("Notas:" + aluno.Notas)
-        console.log("Cursos Matriculados:" + aluno.Cursos);
+        console.log("-".repeat(30));
+        console.log("Nome Do Aluno:" + aluno.Nome);
+        console.log("Notas:" + aluno.Notas);
+        aluno.Cursos.forEach(aluno =>{
+            console.log("Nome do curso:" + aluno.NomeDoCurso);
+            console.log("Data da matricula:" + aluno.dataMatricula);
+        })
         console.log("Faltas:" + aluno.Faltas);
-        console.log("-".repeat(25));
     })
 }
-
-//busca se o aluno esta cadastrado no sistema e emite um feedback, se encontrar aluno ou não;
+//recebe um nome de um aluno como parametro e verifica se o mesmo esta cadastrado no sistema;
+//emite um feedback, caso esteja cadastrado ou não;
 const buscarAluno = nome =>{
-    let alunoAchado = alunosDaEscola.filter(function (aluno) {
-        return aluno.Nome === nome })
-    if (alunoAchado.length > 0) {
-        console.log("-".repeat(25));
-        console.log ("Aluno Encontrado!")
-    }else{
-        console.log ("Aluno não  cadastrado no sistema!")
-    }
+    let alunoAchado = alunosDaEscola.filter(aluno =>{
+        return aluno.Nome === nome });
+    alunoAchado.length > 0 ? console.log ("Aluno Encontrado!") : console.log ("Aluno não  cadastrado no sistema!");
     alunoAchado.forEach((aluno) => {
-        console.log("Nome Do Aluno:" + aluno.Nome)
-        console.log("Notas:" + aluno.Notas)
-        console.log("Cursos Matriculados:" + aluno.Cursos);
+        console.log("-".repeat(30));
+        console.log("Nome Do Aluno:" + aluno.Nome);
+        console.log("Notas:" + aluno.Notas);
+        aluno.Cursos.forEach(aluno =>{
+            console.log("Nome do curso:" + aluno.NomeDoCurso);
+            console.log("Data da matricula:" + aluno.dataMatricula);
+        })
         console.log("Faltas:" + aluno.Faltas);
-        console.log("-".repeat(25));
+        console.log("-".repeat(30));
     })
 }
-//buscarAluno("Carlos Alberto");
+//buscarAluno("Carlos");
 
-const matricularAluno = (nome, curso) =>{
-    let matricula = alunosDaEscola.map(function(){
-            return nome.Cursos = curso
-    })
-return matricula != undefined;
+//recebe como parametro um aluno e o curso desejado, se aluno estiver cadastrado,sera matriculado no curso com sucesso;
+//caso não esteja cadastrado emitira um feedback, avisando que aluno não esta cadastrado;
+const matricularAluno = (aluno, curso) =>{
+    let alunoCadastrado = alunosDaEscola.filter(listaAluno => listaAluno.Nome == aluno.Nome)
+    if(alunoCadastrado.length > 0){
+        aluno.Cursos.push({NomeDoCurso:curso, dataMatricula:new Date()});
+        console.log("Aluno:" + aluno.Nome + " " + "foi Matrículado no curso:" + curso);
+    }else{
+       console.log("não foi possivel realizar matricula, aluno nao cadastrado");
+    }
+
 }
-matricularAluno(alunosDaEscola[0],"Web-Fullstack");
-listarAlunos();
+//matricularAluno(alunosDaEscola[1],"Web-Fullstack");
+
+//recebe um objeto aluno como parametro no qual deseja aplicar falta; e incrementa +1 falta para o aluno;
+const aplicarFalta = aluno =>{
+    if(aluno.Cursos[0]){
+        aluno.Faltas++;
+        console.log("Falta Aplicada com sucesso")
+    }else{                                                                                                                                                            
+        console.log("Não Foi possivel aplicar falta, aluno nao esta matriculado!")
+    }
+}
+//aplicarFalta(alunosDaEscola[1])
+
+//aplica nota para um aluno devidamente matriculado em um curso;
+const aplicarNota = (aluno, nota) =>{ 
+    if(aluno.Cursos[0]){
+        aluno.Notas.push(nota);
+        console.log("Aluno:" + aluno.Nome + " Nota:" + nota + " Aplicada com sucesso!")
+    }else{
+        console.log("Não foi possivel aplicar nota, aluno não esta cadastrado no sistema!")
+    }
+}
+//aplicarNota(alunosDaEscola[1],10)
+
+//aprova aluno, se o mesmo estiver cadastrado no sistema e possuir media de notas maior ou igual 7 e faltas menor ou igual 3;
+const aprovarAluno = aluno =>{
+    if(aluno.Cursos[0]){
+        const mediaAluno = aluno.Notas.reduce((soma, nota) => soma + nota) / aluno.Notas.length
+        const valorArrendondado = parseFloat(mediaAluno.toFixed(2))
+        if(mediaAluno >= 7 && aluno.Faltas <= 3){
+            console.log("Aluno:" + aluno.Nome + " APROVADO!!")
+            console.log("Média do Aluno:" + valorArrendondado)
+        }else{
+            console.log("Aluno:" + aluno.Nome + " REPROVADO!")
+            console.log("Média do Aluno:" + valorArrendondado)
+            console.log("Faltas do aluno:" + aluno.Faltas)
+        }
+    }else{
+        console.log("Aluno Não Esta Matriculado no sistema!")
+    }
+}
+//aprovarAluno(alunosDaEscola[1])
